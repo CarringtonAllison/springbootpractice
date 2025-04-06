c28eb733ae61ddd1cc6dae40419bef9f4cf319bb
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2025-04-06 16:55:53 -0500

    initial commit

---
+++ src/test/java/com/codewithrain/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithrain.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
